7b5761ca77b965acab4e13f6462c734020f96eda
Aaron Scherer <aequasi@gmail.com>
2026-09-24 14:14:34 -0500

    revert(rimworld): require concord again in NoSteamPopup

--- packages/rimworld/mods/NoSteamPopup/Source/NoSteamPopup/NoSteamPopupMod.cs
+++ packages/rimworld/mods/NoSteamPopup/Source/NoSteamPopup/NoSteamPopupMod.cs
@@ -1,4 +1,4 @@
-using System.Reflection;
+using Concord;
 using Verse;
 using Verse.Steam;
 
@@ -8,13 +8,13 @@ public class NoSteamPopupMod : Mod
 {
     public NoSteamPopupMod(ModContentPack content) : base(content)
     {
-        var field = typeof(SteamManager).GetField("initializedInt", BindingFlags.Static | BindingFlags.NonPublic);
-        if (field is null || field.FieldType != typeof(bool))
-        {
-            Log.Warning("[NoSteamPopup] SteamManager.initializedInt is gone; the Steam dialog will still appear.");
-            return;
-        }
-
-        field.SetValue(null, true);
+        Patcher.Apply(typeof(NoSteamPopupMod).Assembly);
     }
+}
+
+[Patch]
+internal abstract class UIRoot_Entry_Patch : UIRoot_Entry
+{
+    [Inject(nameof(UIRoot_Entry.Init), typeof(SteamManager), nameof(SteamManager.Initialized), At.Around)]
+    private bool AlwaysInitialized(Operation<bool> read) => true;
 }
